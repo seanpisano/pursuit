df31617636c0aed193f4b7bffe23acc69904636b
Sean Pisano <sean@seanpisano.com>
2026-03-28 15:40:13 -0400

    last class commit

---
+++ AFYM-Sandbox/assets/js/recipes.js
@@ -0,0 +1,9 @@
+const RECIPES = [
+  'chicken-stock-broth',
+  'shepherds-pie',
+  'buttermilk-brine',
+  'country-gravy-sausage',
+  'bourbon-brown-sugar-bbq-sauce',
+  'buttermilk-biscuits',
+  'zesty-holiday-dip'
+];
